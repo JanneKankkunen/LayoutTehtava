7fce74001b0855b18eb8f12e7930487ab719182f
Janne <janneonkiva@gmail.com>
2020-01-21 22:54:03 +0200

    iyhögpw2q

--- app/src/main/java/com/example/layouttehtava/MainActivity.java
+++ app/src/main/java/com/example/layouttehtava/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.layouttehtava;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button button1 = findViewById(R.id.button1);
+    private Button button2 = findViewById(R.id.button2);
+    private Button button3 = findViewById(R.id.button3);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+            }
+        });
     }
 }
